2bf0b8e89311319c810acfb30dc3cb094afbee91
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-05-09 16:40:34 +0600

    facebook share

--- components/share/share.jsx
+++ components/share/share.jsx
@@ -4,7 +4,7 @@ import { FaFacebook } from "react-icons/fa";
 const ShareOnFacebook = ({ url, title, description, image }) => {
   return (
     <FacebookShareButton url={url} quote={title} hashtag="#YourHashtag">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
+      <button className="facebook__share bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex items-center">
         <FaFacebook className="mr-2" />
         Share on Facebook
       </button>
