e76c1f7babdd25ed63a13287357d4d7519769e8b
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-06-06 12:31:26 +0600

    audiobook created

---
+++ components/AudioBook/AudioBookHome.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const AudioBookHome = () => {
+    return (
+        <section className='hm__audio__book__sec'>
+            <div className='container'>
+                <div className='hm__audio__book__head'>
+                    <a href='#'><img src='/images/audio-logo.png' alt='Audio Logo'/></a>
+                    <p>বই হচ্ছে শ্রেষ্ঠ আত্মীয়, যার সঙ্গে কোনদিন ঝগড়া হয় না,কোনদিন মনোমালিন্য হয় না। - প্রতিভা বসু</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default AudioBookHome;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-
 import Head from 'next/head';
-import Procchod from '../components/procchod/Procchod'
-import { ImageSlider } from '../components/slider/ImageSlider'
+import Procchod from '../components/procchod/Procchod';
+import { ImageSlider } from '../components/slider/ImageSlider';
+import AduioBookHome from '../components/AudioBook/AudioBookHome';
 
 export default function Home() {
 
@@ -14,6 +14,7 @@ export default function Home() {
 
         <div className="hm__slider__wrp lg:pt-[95px] md:pt-[90px] sm:pt-[92px] text-black " oncontextmenu="return false;">
           <ImageSlider />
+          <AduioBookHome />
           <Procchod />
         </div>
       </div>
