3bd3a70af505e20e090a9899492b27b9709ca9dc
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-06-06 17:14:04 +0600

    btn fix

--- components/slider/ImageSlider.jsx
+++ components/slider/ImageSlider.jsx
@@ -101,7 +101,7 @@ export function ImageSlider() {
                                         <div className="slider__desc">
                                             <h1 className="lg:text-[52px] md:text-[48px] sm:text-[44px] xs:text-[38px] text-[#f58807]" >{title}</h1>
                                             <h2 className="lg:text-[28px] md:text-[24px] sm:text-[22px] xs:text-[18px] text-[#595D5B]">{caption}</h2>
-                                            <p className="text-[16px] text-[#595D5B] w-[90%]">{content}</p>
+                                            <p className="lg:text-[18px] md:text-[17px] sm:text-[16px] xs:text-[14px] text-[#595D5B] w-[90%]">{content}</p>
 
                                             <button
                                                 onClick={() => featureHandler(_id)}
