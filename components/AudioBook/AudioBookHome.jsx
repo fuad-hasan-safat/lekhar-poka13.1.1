c1eaf83718bfe78423ee4b640d115b08e217000a
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-06-11 11:48:56 +0600

    fff

--- components/AudioBook/AudioBookHome.jsx
+++ components/AudioBook/AudioBookHome.jsx
@@ -4,8 +4,10 @@ const AudioBookHome = () => {
     return (
         <section className='hm__audio__book__sec'>
             <div className='container'>
-                <div className='hm__audio__book__head'>
-                    <a href='#'><img src='/images/audio-logo.png' alt='Audio Logo'/></a>
+                <div className='hm__audio__book__wrap'>
+                    <div className='audiu__logo'>
+                        <a href='#'><img src='/images/audio-logo.png' alt='Audio Logo'/></a>
+                    </div>
                     <p>বই হচ্ছে শ্রেষ্ঠ আত্মীয়, যার সঙ্গে কোনদিন ঝগড়া হয় না,কোনদিন মনোমালিন্য হয় না। - প্রতিভা বসু</p>
                 </div>
             </div>
