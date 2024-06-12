9dc8288efff62bd92f89d7c7a1dd240b621f87ae
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-06-12 11:22:42 +0600

    playlist json created

---
+++ components/AudioBook/components/audioPlaylist/PlaylistItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function PlaylistItem({songInfo}) {
+  return (
+    <div className='flex flex'>
+
+    </div>
+  )
+}
---
+++ components/AudioBook/components/sampleData/audioPlaylist.js
@@ -0,0 +1,32 @@
+export const audioPlaylist = [
+    {
+        title: 'বাগিচায় বুলবুলি তুই',
+        audio: '/audioBook/audioFileDemo/Bulbuli  Coke Studio Bangla  Season One  Ritu Raj X Nandita.mp3',
+        image: '/audioBook/demoCatImage/bookCover.jpg',
+    },
+    {
+        title: 'ও সঙ্গী',
+        audio: '/audioBook/audioFileDemo/O Sangi  Sukheri Chowate  Lyrical Song  Mita Chatterjee  Bengali Hit Songs  Atlantis Music.mp3',
+        image: '/audioBook/demoCatImage/cover 1.jpg',
+    },
+    {
+        title: 'ফেরো নাজারিয়া',
+        audio: '/audioBook/audioFileDemo/Phero Na Najariya.mp3',
+        image: '/audioBook/demoCatImage/cover 3.webp',
+    },
+    {
+        title: 'বাগিচায় বুলবুলি তুই',
+        audio: '/audioBook/audioFileDemo/Bulbuli  Coke Studio Bangla  Season One  Ritu Raj X Nandita.mp3',
+        image: '/audioBook/demoCatImage/bookCover.jpg',
+    },
+    {
+        title: 'ও সঙ্গী',
+        audio: '/audioBook/audioFileDemo/O Sangi  Sukheri Chowate  Lyrical Song  Mita Chatterjee  Bengali Hit Songs  Atlantis Music.mp3',
+        image: '/audioBook/demoCatImage/cover 1.jpg',
+    },
+    {
+        title: 'ফেরো নাজারিয়া',
+        audio: '/audioBook/audioFileDemo/Phero Na Najariya.mp3',
+        image: '/audioBook/demoCatImage/cover 3.webp',
+    },
+]
