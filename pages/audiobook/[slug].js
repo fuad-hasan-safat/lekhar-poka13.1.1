e50f257247e747d109508ceddd480d53947b0ddf
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-06-13 11:55:47 +0600

    Update [slug].js

--- pages/audiobook/[slug].js
+++ pages/audiobook/[slug].js
@@ -12,10 +12,18 @@ export default function Home() {
                     <title>Single AUdio</title>
                 </Head>
 
+                <section className='banner-sec-wrap'>
+                    <div className="relative w-full xl:h-[380px] lg:h-[380px] md:h-[360px] sm:h-[280px] xs:h-[270px]  overflow-hidden" style={{ background: `url('/images/pages-banner-svg/baseBanner.png')center center / cover no-repeat` }}>
+                    </div>
+
+                </section>
+
                 <div className='container flex flex-row lg:pt-[95px] md:pt-[90px] sm:pt-[92px]'>
-                    <div className="w-[70%] hm__slider__wrp  text-black " oncontextmenu="return false;">
+
+                    <div className='w-[70%]'>
 
                     </div>
+
 
                     <div className='w-[30%]'>
                         <AudioDetailsSideBar />
