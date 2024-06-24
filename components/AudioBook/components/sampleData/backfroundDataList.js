608cd4a13e41870383a1702cac04bb41b50635d6
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-06-24 16:27:06 +0600

    slider update

---
+++ components/AudioBook/components/sampleData/backfroundDataList.js
@@ -0,0 +1,64 @@
+export const bgAudioList = {
+    background: 'background',
+    category: 'সমসাময়িক',
+    data:
+        [
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#00736B',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#E43727',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#FFBF00',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#59C1B9',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#585EDF',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#E13426',
+            },
+            {
+                image: '/audioBook/audio-slider/bgSliderImg.png',
+                title: 'তিন গোয়েন্দা',
+                writer: 'রকিব হাসান',
+                voice: 'রকিব হাসান তুষার',
+                duration: '১:৪১ মিনিট',
+                color: '#D35400',
+            },
+
+        ]
+}
