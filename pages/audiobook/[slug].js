bd3677d0d4f3bc24b8a2f3fab0cdd9716d7abe92
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-06-25 18:00:32 +0600

    audio page responsive done

--- pages/audiobook/[slug].js
+++ pages/audiobook/[slug].js
@@ -12,7 +12,7 @@ export default function Home() {
                     <title>Single AUdio</title>
                 </Head>
 
-                <section className='banner-sec-wrap pt-[96px] '>
+                <section className='banner-sec-wrap lg:pt-[96px] md:pt-[75 px]'>
                     <div className="relative  flex justify-center items-center  w-full xl:h-[380px] lg:h-[380px] md:h-[360px] sm:h-[280px] xs:h-[270px]  overflow-hidden" style={{ background: `url('/images/pages-banner-svg/baseBanner.png')center center / cover no-repeat` }}>
                         <div className='audio_banner__info__wrap flex flex-row place-content-center justify-center items-center'>
                             <div className=''>
@@ -20,9 +20,9 @@ export default function Home() {
 
                             </div>
                             <div className='ml-[98px] text-center relative'>
-                                <h5  className='text-[#F9A106] text-[48px] font-semibold'>{singleAudioData?.title}</h5>
+                                <h5  className='text-[#F9A106] lg:text-[48px] md:text-[45px] sm:text-[42px] xs:text-[38px] font-semibold'>{singleAudioData?.title}</h5>
                                 <img className='absolute -top-[10px] -right-[30px] ' src={singleAudioData?.category_icon}/>
-                                <h5 className='text-[#484848] text-[28px] font-semibold'>{singleAudioData.type} | {singleAudioData.writer} </h5>
+                                <h5 className='text-[#484848] lg:text-[28px] md:text-[26px] sm:text-[24px] xs:text-[20px] font-semibold'>{singleAudioData.type} | {singleAudioData.writer} </h5>
                                 <div className='flex flex-row text-center place-content-center'><img src='/audioBook/listiningIcon.png'/> 
                                 <p className='text-[16px] text-[#484848] ml-[4px]'>{singleAudioData?.lisening_time}</p></div>
 
@@ -33,14 +33,14 @@ export default function Home() {
 
                 </section>
 
-                <div className='container flex flex-row lg:pt-[95px] md:pt-[90px] sm:pt-[92px]'>
+                <div className='container lg:flex md:flex sm:block xs:block flex-row lg:pt-[95px] md:pt-[90px] sm:pt-[92px]'>
 
-                    <div className='w-[70%]'>
+                    <div className='lg:w-[70%] md:w-[70%] sm:w-full xs:w-full'>
                         <AudioTabs audioData={singleAudioData} />
                     </div>
 
 
-                    <div className='w-[30%]'>
+                    <div className='lg:w-[30%] md:w-[30%] sm:w-full xs:w-full'>
                         <AudioDetailsSideBar />
                     </div>
 
