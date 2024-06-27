35d7333504ec96171dd882c63e3c1e93184bf635
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-06-27 18:04:49 +0600

    beta added

--- lekha-poka-ui-beta.js
+++ lekha-poka-ui-beta.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const next = require('next');
-const port = 3009;
+const port = 3007;
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({dev});
 const handle = app.getRequestHandler();
