1ef6a4bd03db9b0d580030d60935d923d72af6bb
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-06-30 11:26:48 +0600

    df

--- components/AudioBook/AudioPlayer/AudioPlayer.jsx
+++ components/AudioBook/AudioPlayer/AudioPlayer.jsx
@@ -119,135 +119,102 @@ export default function AudioPlayer() {
 
   return createPortal((
     <>
-      <div className="audio-player-wrap fixed text-black  backdrop-blur-lg  place-content-center text-center justify-center bottom-[0] bg-yellow-500/30  w-full h-[140px] z-[999999]">
-        <div className="container lg:flex lg:flex-row justify-center content-center lg:space-x-16">
-          <div className="items-center content-center justify-center z-[200] w-[300px]">
-            {/* song info */}
-            <div className="lg:flex lg:flex-row lg:w-[380px] space-x-2  md:hidden sm:hidden xs:hidden">
-
-              <div className="">
-                <img
-                  src={currentSong?.image}
-                  alt={currentSong?.title}
-                  width={70}
-                  height={70}
-                  className="h-[70px] w-[70px] rounded-full"
-                ></img>
-              </div>
-
-              <div className="lg:flex lg:flex-col text-gray-600 lg:space-y-2">
-
-                <div className="">
-                  <div className="text-xl  font-bold">{currentSong?.title}</div>
+           <div className="audio-player-wrap fixed text-black  backdrop-blur-lg  text-center bottom-[0] bg-yellow-500/30  w-full z-[999999]">
+        <div className="container">
+            <div className="audio__player__innr">
+              <div className="items-center content-center justify-center z-[200] w-[300px]">
+                <div className="lg:flex lg:flex-row lg:w-[380px] justify-left items-center md:hidden sm:hidden xs:hidden">
+                  <div className="">
+                    <img
+                      src={currentSong?.image}
+                      alt={currentSong?.title}
+                      width={70}
+                      height={70}
+                      className="h-[70px] w-[70px] rounded-full"
+                    ></img>
+                  </div>
+                  <div className="lg:flex lg:flex-col text-gray-600 pt-[5px] pl-[10px]">
+                    <div className="pb-[5px]">
+                      <div className="text-xl  font-bold text-left">{currentSong?.title}</div>
+                    </div>
+                    <div>
+                      <div className="text-left">{currentSong?.writer}</div>
+                    </div>
+                  </div>
                 </div>
-
+              </div>
+              <div className="flex flex-col text-center items-center content-center justify-center space-y-3  lg:w-full">
                 <div>
-                  <div className="text-left">{currentSong?.writer}</div>
+                  <audio
+                    src={currentSong.src}
+                    ref={audioPlayer}
+                    onTimeUpdate={handleTimeUpdate}
+                    onLoadedMetadata={handleLoadedMetadata}
+                    onEnded={handleEnded}
+                  />
+                  <div className="flex flex-row  space-x-6">
+                    <button onClick={toggleShuffle} className={isShuffle ? "active" : ""}>
+                      <MdShuffle />
+                    </button>
+                    <button onClick={handlePreviousSong}>
+                      <MdSkipPrevious />
+                    </button>
+                    <button className="text-4xl" onClick={togglePlay}>
+                      {isPlaying ?
+                        //  <MdPause /> 
+                        <>
+                          <img
+                            src="/images/icons/ic_pause.svg"
+                          ></img>
+                        </>
+                        :
+                        <MdPlayArrow
+                        />}
+                    </button>
+                    <button onClick={handleNextSong}>
+                      <MdSkipNext />
+                    </button>
+                    <button onClick={toggleRepeat} className={isRepeat ? "active" : ""}>
+                      <MdRepeat />
+                    </button>
+                  </div>
                 </div>
-
+                <div className="audio-sound-line flex items-center flex-row text-center place-content-center justify-center lg:space-x-5 md:space-x-5 sm:space-x-3 xs:space-x-3 lg:pb-[15px] md:pb-[15px] sm:pb-[10px] xs:pb-[10px]">
+                  <span className="text-xs">{formatTime(currentTime)}</span>
+                  <ProgressBar
+                    now={(currentTime / duration) * 100}
+                    style={{ width: "300px", height: "6px", backgroundColor: 'white', borderRadius: '5px', cursor: 'pointer' }}
+                    onClick={handleProgressClick}
+                  />
+                  <span className="text-xs">{formatTime(duration)}</span>
+                </div>
               </div>
-
-            </div>
-
-          </div>
-
-          <div className="flex flex-col text-center items-center content-center justify-center space-y-3  lg:w-full">
-            {/* audio player */}
-            <div>
-
-              <audio
-                src={currentSong?.audio}
-                ref={audioPlayer}
-                onTimeUpdate={handleTimeUpdate}
-                onLoadedMetadata={handleLoadedMetadata}
-                onEnded={handleEnded}
-              />
-
-              <div className="flex flex-row  space-x-6">
-
-                <button onClick={toggleShuffle} className={isShuffle ? "active" : "text-gray-400"}>
-                  <MdShuffle />
-                </button>
-
-                <button onClick={prevSongPlay}>
-                  <MdSkipPrevious />
-                </button>
-
-                <button className="text-4xl" onClick={()=>toggleAudioPlay(currentPlayingIndex, playList, audioPlace)}>
-                  {isAudioPlaying ?
-                    //  <MdPause /> 
-                    <>
-                      <img
-                        src="/images/icons/ic_pause.svg"
-                      ></img>
-                    </>
-
-                    :
-                    <MdPlayArrow
-                    />}
-                </button>
-
-                <button onClick={nextSongPlay}>
-                  <MdSkipNext />
-                </button>
-
-                <button onClick={toggleReapet} className={isRepeat ? "active" : "text-gray-400"}>
-                  <MdRepeat />
-                </button>
-
+              <div className="w-full text-center lg:pl-[20px] md:pl-[20px] sm:pl-[0] xs:pl-[0] items-center content-center justify-center">
+                {/* left part */}
+                <div className="flex lg:justify-left md:justify-left sm:justify-center xs:justify-center items-center flex-row  space-x-5 ">
+                  {/* fav icon */}
+                  <button className="">
+                    <img src="/images/icons/ic_fav.svg"></img>
+                  </button>
+                  {/* volume icon */}
+                  {!isMute && <img onClick={() => { setIsMute(true); audioPlayer.current.volume = 0; }} width={30} height={30} src="/images/icons/ic_volumeon.svg"></img>}
+                  {isMute && <img onClick={() => { setIsMute(false); audioPlayer.current.volume = volume }} width={30} height={30} src="/images/icons/ic_volumeoff.svg"></img>}
+                  {/* volume bar */}
+                  <div className="text-center items-center content-center justify-center ">
+                    <ProgressBar
+                      // variant="customBarColor"
+                      now={volume * 100}
+                      style={{ width: "100px", height: "6px", backgroundColor: 'white', borderRadius: '5px', cursor: 'pointer' }}
+                      onClick={handleVolumeClick}
+                    />
+                  </div>
+                  {/*   notes */}
+                  <button>
+                    <img src="/images/icons/ic_songlist.svg"></img>
+                  </button>
+                </div>
               </div>
-
-            </div>
-
-            <div className="audio-sound-line flex flex-row text-center place-content-center justify-center space-x-5 pb-[15px]">
-
-              <span className="text-xs">{formatTime(currentTime)}</span>
-
-              <ProgressBar
-                now={(currentTime / duration) * 100}
-                style={{ width: "300px", height: "6px", backgroundColor: 'white', borderRadius: '5px', cursor: 'pointer' }}
-                onClick={handleProgressClick}
-              />
-
-              <span className="text-xs">{formatTime(duration)}</span>
-
-            </div>
-
-          </div>
-
-          <div className="w-full text-center items-center content-center justify-center">
-            {/* left part */}
-
-            <div className="flex flex-row  space-x-5 ">
-              {/* fav icon */}
-              <button className="">
-                <img src="/images/icons/ic_fav.svg"></img>
-              </button>
-
-              {/* volume icon */}
-
-              {!isMute && <img className="cursor-pointer" onClick={() => { setIsMute(true); audioPlayer.current.volume = 0; }} width={30} height={30} src="/images/icons/ic_volumeon.svg"></img>}
-              {isMute && <img className="cursor-pointer" onClick={() => { setIsMute(false); audioPlayer.current.volume = volume }} width={30} height={30} src="/images/icons/ic_volumeoff.svg"></img>}
-
-              {/* volume bar */}
-              <div className="text-center items-center content-center justify-center ">
-
-                <ProgressBar
-                  // variant="customBarColor"
-                  now={volume * 100}
-                  style={{ width: "100px", height: "6px", backgroundColor: 'white', borderRadius: '5px', cursor: 'pointer' }}
-                  onClick={handleVolumeClick}
-                />
-
-              </div>
-              {/*   notes */}
-
-              <button>
-                <img src="/images/icons/ic_songlist.svg"></img>
-              </button>
-
-            </div>
-          </div>
+           </div>
         </div>
       </div>
     </>
