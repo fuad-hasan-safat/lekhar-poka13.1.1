68428e63153768a39eb759e6111430889b992187
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2024-07-03 13:50:31 +0600

    dashboard page done

--- components/Dashboard/components/DashboardContent.js
+++ components/Dashboard/components/DashboardContent.js
@@ -4,65 +4,171 @@ import React from 'react';
 const dashboardCtData=[
     {
         title:'কবিতা',
-        text:'Total Kobita',
+        text:'মোট কবিতা সংখ্যা',
         number:'100',
         color:'#f58807',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'উপন্যাস',
+        text:'মোট উপন্যাস সংখ্যা',
         number:'85',
         color:'#00CFE8',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'প্রবন্ধ',
+        text:'মোট প্রবন্ধ সংখ্যা',
         number:'90',
         color:'#FCD200',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'গল্প',
+        text:'মোট গল্প সংখ্যা',
         number:'200',
-        color:'#1B2850',
+        color:'#fcab36',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'জীবনী',
+        text:'মোট জীবনী সংখ্যা',
         number:'250',
-        color:'#FF9F43',
+        color:'#00736B',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'ছোট গল্প',
+        text:'মোট ছোট গল্প সংখ্যা',
         number:'230',
-        color:'#FF9F43',
+        color:'#5DE3FF',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'অনুগল্প',
+        text:'মোট অনুগল্প সংখ্যা',
         number:'120',
         color:'#FF9F43',
     },
     {
-        title:'কবিতা',
-        text:'Total Kobita',
+        title:'শিশুসাহিত্য',
+        text:'মোট শিশুসাহিত্য সংখ্যা',
         number:'150',
-        color:'#FF9F43',
+        color:'#ECFF43',
+    },
+    {
+        title:'বিজ্ঞান ',
+        text:'মোট বিজ্ঞান সংখ্যা',
+        number:'150',
+        color:'#28C76F',
+    },
+    {
+        title:'ইসলামিক ',
+        text:'মোট ইসলামিক সংখ্যা',
+        number:'150',
+        color:'#F96E6F',
+    },
+]
+
+const AudioCategoryData=[
+    {
+        title:'রোমান্টিক',
+        text:'মোট রোমান্টিক সংখ্যা',
+        number:'100',
+        color:'#FF5555',
+    },
+    {
+        title:'সাইন্স ফিকশন',
+        text:'মোট সাইন্স ফিকশন সংখ্যা',
+        number:'85',
+        color:'#59C1B9',
+    },
+    {
+        title:'উপন্যাস',
+        text:'মোট উপন্যাস সংখ্যা',
+        number:'90',
+        color:'#585EDF',
+    },
+    {
+        title:'কবিতা',
+        text:'মোট কবিতা সংখ্যা',
+        number:'200',
+        color:'#00D67C',
+    },
+    {
+        title:'কিশোর উপন্যাস',
+        text:'মোট কিশোর উপন্যাস সংখ্যা',
+        number:'250',
+        color:'#F247C2',
+    },
+    {
+        title:'ভ্রমণ',
+        text:'মোট ছোট গল্প সংখ্যা',
+        number:'230',
+        color:'#B741FF',
+    },
+    {
+        title:'জীবনী',
+        text:'মোট জীবনী সংখ্যা',
+        number:'120',
+        color:'#FC3939',
+    },
+    {
+        title:'গল্প',
+        text:'মোট গল্প সংখ্যা',
+        number:'150',
+        color:'#FFC700',
+    },
+    {
+        title:'ইতিহাস ',
+        text:'মোট ইতিহাস সংখ্যা',
+        number:'150',
+        color:'#FFC700',
+    },
+    {
+        title:'ইসলামিক ',
+        text:'মোট ইসলামিক সংখ্যা',
+        number:'150',
+        color:'#585EDF',
+    },
+    {
+        title:'রোমান্টিক ',
+        text:'মোট রোমান্টিক সংখ্যা',
+        number:'150',
+        color:'#59C1B9',
+    },
+    {
+        title:'ক্লাসিকস ',
+        text:'মোট ক্লাসিকস সংখ্যা',
+        number:'150',
+        color:'#B741FF',
     },
 ]
 
 function DashboardContent() {
     return (
         <div className='dashboard__component'>
-            <div className='dashboard__category'>
+            <div className='dashboard__category clearfix'>
+                <h3>Lekharpoka</h3>
                 <ul>
                     {dashboardCtData.map((item,index)=>(
                       <li>
                         <div className='dashboard__cmpt__grid' style={{backgroundColor:`${item.color}`}}>
+                            <div>
+                                <h5>{item.title}</h5>
+                                <p>{item.text}</p>
+                            </div>
                             <strong>{item.number}</strong>
-                            <h5>{item.title}</h5>
-                            <p>{item.text}</p>
+                        </div>
+                      </li>
+                    ))}
+                </ul>
+            </div>
+            <div className='dashboard__category clearfix'>
+                <h3>Audio Book</h3>
+                <ul>
+                    {AudioCategoryData.map((item,index)=>(
+                      <li>
+                        <div className='dashboard__cmpt__grid' style={{backgroundColor:`${item.color}`}}>
+                            <div>
+                                <h5>{item.title}</h5>
+                                <p>{item.text}</p>
+                            </div>
+                            <strong>{item.number}</strong>
                         </div>
                       </li>
                     ))}
