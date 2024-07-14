e7b81ff60ccd6cc1712808f194178a4de37e3fb6
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-07-14 17:43:17 +0600

    Update userPostTitleAndcover.jsx

--- components/common/userPostTitleAndcover.jsx
+++ components/common/userPostTitleAndcover.jsx
@@ -105,7 +105,7 @@ export default function UserPostTitleAndcover({
   let bannerImage = image;
 
   if (image === undefined || image === null || image === 'undefined') {
-    bannerImage = ''
+    bannerImage = writerImage;
   }
 
   let writerImage = writer_image
