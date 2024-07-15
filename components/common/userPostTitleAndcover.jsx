20c117008c8e641e9368d6a56122577e42ad5ad4
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-07-15 14:12:36 +0600

    sdvd

--- components/common/userPostTitleAndcover.jsx
+++ components/common/userPostTitleAndcover.jsx
@@ -150,7 +150,7 @@ export default function UserPostTitleAndcover({
 
         <div className="profile__auth__img">
 
-          <img className="object-cover lg:w-[120px] lg:h-[120px] md:w-[110px] md:h-[110px] sm:w-[100px] sm:h-[100px] xs:w-[80px] xs:h-[80px] rounded-[16px] block m-auto" src={bannerImage === '' ? defaultBannerImage : `${apiBasePath}/${bannerImage}`} alt="" />
+          <img className="object-cover lg:w-[120px] lg:h-[120px] md:w-[110px] md:h-[110px] sm:w-[100px] sm:h-[100px] xs:w-[80px] xs:h-[80px] rounded-[16px] block m-auto" src={bannerImage === '' ? defaultBannerImage : `${apiBasePath}/${bannerImage?.slice(bannerImage?.indexOf('/') + 1)}`} alt="" />
 
         </div>
 
@@ -161,7 +161,7 @@ export default function UserPostTitleAndcover({
 
           <Link className="flex items-center lg:text-[18px] md:text-[16px] sm:text-[14px] xs:text-[12px]  font-semibold text-[#595D5B] " href={`/postswriter/${writer_id}`} > 
           <span className='inline-block mr-[10px]'>
-            <img className="lg:w-[24px] lg:h-[24px] md:w-[22px] md:h-[22px] sm:w-[20px] sm:h-[20px] xs:w-[18px] xs:h-[18px] rounded-full block m-auto shadow-lg" src={writerImage === '' ? defaultBannerImage : `${apiBasePath}/${writerImage}`} alt="" />
+            <img className="lg:w-[24px] lg:h-[24px] md:w-[22px] md:h-[22px] sm:w-[20px] sm:h-[20px] xs:w-[18px] xs:h-[18px] rounded-full block m-auto shadow-lg" src={writerImage === '' ? defaultBannerImage : `${apiBasePath}/${writerImage?.slice(writerImage.indexOf('/') + 1)}`} alt="" />
           </span>
           
           <span className='inline-block'> {shortenWriter} </span></Link>
