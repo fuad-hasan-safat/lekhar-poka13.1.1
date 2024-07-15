567848082c5b040de13dfb8044e3ee2e441bcd40
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-07-15 16:02:38 +0600

    Update userPostTitleAndcover.jsx

--- components/common/userPostTitleAndcover.jsx
+++ components/common/userPostTitleAndcover.jsx
@@ -162,7 +162,7 @@ export default function UserPostTitleAndcover({
 
         <div className="hm__post__profile__grid  relative">
           <div className="">
-            <h1 className="lg:text-[32] md:text-[25px] sm:text-[23px] xs:text-[14px] leading-7 lg:pr-[50px] text-[#FCD200] font-bold" style={{ lineHeight: '1.2' }}>{shortenTitle}</h1>
+            <h1 className="lg:text-[32] md:text-[25px] sm:text-[23px] xs:text-[14px] leading-7 pr-[40px] text-[#FCD200] font-bold" style={{ lineHeight: '1.2' }}>{shortenTitle}</h1>
           </div>
 
           <Link className="flex items-center lg:text-[18px] md:text-[16px] sm:text-[14px] xs:text-[12px]  font-semibold text-[#595D5B] " href={writerClickLink} >
