1af62de7c03e695016e7f0439bb449b5e14b6364
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-07-31 14:39:26 +0600

    Update DeleteEbookAndAudio.js

--- components/Dashboard/components/DeleteEbookAndAudio.js
+++ components/Dashboard/components/DeleteEbookAndAudio.js
@@ -1,26 +1,32 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { apiBasePath } from '../../../utils/constant';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import DialugueModal from '../../common/notification/DialugueModal';
 
 export default function DeleteEbookAndAudio() {
 
     let notification = '';
-
+    const dialogueRef = useRef();
     const [audioData, setAudioData] = useState({
         audioId: '',
+        audioTitle: '',
         ebook_id: '',
+        ebookTitle: '',
+        deleteType:''   // 'audio' or 'ebook'
     });
 
     const [allBooks, setAllBooks] = useState([]);
     const [audiosOfSelectedBook, setAudioOfSelectedBook] = useState([]);
+    const [deleteMessage, setDeleteMessage] = useState('');
+
     useEffect(() => {
         getAudioBookList()
     }, [])
 
     useEffect(() => {
-        if (audioData.ebook_id.length > 0) {
+        if (audioData.ebook_id?.length > 0) {
             getAudioList()
         }
     }, [audioData.ebook_id])
@@ -63,26 +69,125 @@ export default function DeleteEbookAndAudio() {
         const { name, value, type, files } = event.target;
 
         if (name === 'audio') {
+            const arr = value.split(',');
+            console.log({value, arr});
             setAudioData((prevData) => ({
                 ...prevData,
-                audioId: value
+                audioId: arr[0],
+                audioTitle: arr[1]
             }))
         }
 
         if (name === 'book') {
+            const arr = value.split(',');
+            console.log({value, arr});
             setAudioData((prevData) => ({
                 ...prevData,
-                ebook_id: value
+                ebook_id: arr[0],
+                ebookTitle: arr[1]
             }))
         }
 
     }
 
+    const detele = async () => {
+
+        if(audioData.deleteType === 'audio'){
+
+            axios
+            .delete(`${apiBasePath}/deleteaudio/${audioData.audioId}`)
+            .then((response) => {
+                console.log("Delete successful:", response.data);
+                notification = `${audioData.audioTitle} ডিলিট হয়েছে`;
+                notify1();
+                dialogueRef.current.close();
+            })
+            .catch((error) => {
+                console.error("Error deleting data:", error);
+                notification = `${audioData.audioTitle} ডিলিট হয়নি`;
+                notify();
+            });
+
+        } else if( audioData.deleteType === 'book'){
+            axios
+            .delete(`${apiBasePath}/deletebook/${audioData.ebook_id}`)
+            .then((response) => {
+                console.log("Delete successful:", response.data);
+                notification = `${audioData.ebookTitle} ডিলিট হয়েছে`;
+                notify1();
+                dialogueRef.current.close();
+            })
+            .catch((error) => {
+                console.error("Error deleting data:", error);
+                notification = `${audioData.ebookTitle} অডিও ডিলিট হয়নি`;
+                notify();
+            });
+        }
+
+    }
+
+    const handleDeleteBook = () => {
+
+        if(audioData.ebook_id.length > 0) {
+            setDeleteMessage(`${audioData.ebookTitle} অডিও বুকটি মুছে ফেলতে চান ?`);
+
+            setAudioData((prevData) => ({
+                ...prevData,
+                deleteType: 'book'
+            }))
+
+            dialogueRef.current.showModal();
+           
+        }else{
+            notification = 'অডিও বুক সিলেক্ট করুন';
+            notify();
+        }
+
+    }
+    const handleAudioDelete = () => {
+
+        if (audioData.audioId.length > 0) {
+            setDeleteMessage(`${audioData.audioTitle} অডিওটি  মুছে ফেলতে চান ?`);
+
+            setAudioData((prevData) => ({
+                ...prevData,
+                deleteType: 'audio'
+            }))
+
+            dialogueRef.current.showModal();
+        }else{
+            notification = 'অডিও ফাইল সিলেক্ট করুন';
+            notify();
+        }
+
+    }
+
+
+    const notify1 = () => toast.success(notification, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+
+    });
+    const notify = () => toast.warn(notification, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+
+    });
 
 
     return (
         <div className='admin__add__slider__wrap'>
             <ToastContainer />
+            <DialugueModal ref={dialogueRef} alert={deleteMessage} address={detele} type='delete' />
+
             <div >
                 <div className='audio__book__input__fields clearfix'>
                     <div className='admin__input  text-black'>
@@ -90,18 +195,19 @@ export default function DeleteEbookAndAudio() {
                         <select
                             name="book"
                             id="book"
-                            value={audioData.ebook_id}
+                            value={`${audioData.ebook_id},${audioData.ebookTitle}`}
                             onChange={handleChange}
                         >
                             <option>বই নির্বাচন করুন</option>
                             {allBooks.map((book) => (
-                                <option key={book._id} value={book._id}>
+                                <option key={book._id} value={`${book._id},${book.title}`}>
                                     {book.title}
                                 </option>
                             ))}
                         </select>
                         <div className='pt-[20px]'>
                             <button
+                                onClick={handleDeleteBook}
                                 className="page__common__yello__btn w-[100%] max-w-[300px] h-[50px] bg-[#FCA000] rounded-md text-[16px] text-white items-center profile__btn__midl"
                                 type="submit"
                             >
@@ -115,18 +221,19 @@ export default function DeleteEbookAndAudio() {
                         <select
                             name="audio"
                             id="audio"
-                            value={audioData.audioId}
+                            value={`${audioData.audioId},${audioData.audioTitle}`}
                             onChange={handleChange}
                         >
                             <option>অডিও নির্বাচন করুন</option>
                             {audiosOfSelectedBook.map((audio) => (
-                                <option key={audio._id} value={audio._id}>
+                                <option key={audio._id} value={`${audio._id},${audio.title}`}>
                                     {audio.title}
                                 </option>
                             ))}
                         </select>
                         <div className='pt-[20px]'>
                             <button
+                                onClick={handleAudioDelete}
                                 className="page__common__yello__btn w-[100%] max-w-[300px] h-[50px] bg-[#FCA000] rounded-md text-[16px] text-white items-center profile__btn__midl"
                                 type="submit"
                             >
@@ -138,25 +245,6 @@ export default function DeleteEbookAndAudio() {
                 </div>
             </div>
 
-            {/* <div className='submit__btn flex  !mt-[40px]'>
-                <div className='w-[50%] '>
-                    <button
-                        className="page__common__yello__btn w-[100%] max-w-[300px] h-[50px] bg-[#FCA000] rounded-md text-[16px] text-white items-center profile__btn__midl"
-                        type="submit"
-                    >
-                        বই মুছে ফেলুন
-                    </button>
-                </div>
-
-                <div className='w-[50%] '>
-                    <button
-                        className="page__common__yello__btn w-[100%] max-w-[300px] h-[50px] bg-[#FCA000] rounded-md text-[16px] text-white items-center profile__btn__midl"
-                        type="submit"
-                    >
-                        অডিও মুছে ফেলুন
-                    </button>
-                </div>
-            </div> */}
         </div>
     )
 }
