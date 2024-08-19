304b49beb4cfa0f82df872da1909870fffe04582
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-08-19 15:23:00 +0600

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -23,7 +23,7 @@ export async function getServerSideProps(context) {
 
   } catch (error) {
     postData = {
-      status:'failed'
+      status: 'failed'
     }
   }
 
@@ -87,141 +87,143 @@ export default function PostDetails({ postData }) {
   return (
 
     <>
-    { poststatus ? <>
+      {poststatus ? <>
 
-      <div>
-        <Head>
+        <div>
+          <Head>
 
-          <title>{data?.title}</title>
-          <meta property="og:title" content={pageTitle} key="og:title" />
-          <meta property="og:description" content={`${description} #lekharpoka`} />
-          <meta property="og:image" content={imageLink} key="og:image" />
-          <meta property="og:url" content={postLink} />
-          <meta property="og:type" content="website" key="og:type" />
-          <meta name="twitter:card" content={imageLink} />
-          <meta name="twitter:title" content={pageTitle} />
-          <meta name="twitter:description" content={description} />
-          <meta name="twitter:image" content={imageLink} />
+            <title>{data?.title}</title>
+            <meta property="og:title" content={pageTitle} key="og:title" />
+            <meta property="og:description" content={`${description} #lekharpoka`} />
+            <meta property="og:image" content={imageLink} key="og:image" />
+            <meta property="og:url" content={postLink} />
+            <meta property="og:type" content="website" key="og:type" />
+            <meta name="twitter:card" content={imageLink} />
+            <meta name="twitter:title" content={pageTitle} />
+            <meta name="twitter:description" content={description} />
+            <meta name="twitter:image" content={imageLink} />
 
-        </Head>
-      </div>
-      <div className=" body__control" >
-        <div className="all__post__content__overlay">
-          <section className="banner-sec-wrap place-content-center">
-            <div className="banner__bg__sm__wrap relative w-full xl:h-[190px] lg:h-[180px] md:h-[180px] sm:h-[180px] xs:h-[170px]  overflow-hidden" style={{ background: `url('/images/pages-banner-svg/baseBanner.png')center center / cover no-repeat` }}>
-              {<h2 className=" absolute lg:text-[40px] md:text-[38px] sm:text-[35px] xs:text-[25px] text-[#F9A106]">{data?.category}</h2>}
-            </div>
-          </section>
-          <section className="all__post__main__content">
-            <div className="container">
-              <div className="lg:flex lg:flex-row">
-                {(
-                  <div className="flex flex-col w-full relative z-50">
-                    {isdataFetch &&
-                      <>
-                        <div className="kobita__dsc__lft relative flex lg:mb-[110px] md:mb-[84px]">
-                          <div>
-                            <FullPost
-                              id={slug}
-                              content={data?.content}
-                              title={data?.title}
-                              writer={data?.writer}
-                              writer_id={data?.writer_id}
-                              image={selectedCoverImage}
-                              uploadedBy={profileName}
-                              writer_image={writerImage}
-                              profileName={profileName}
-                              updatedAt={data?.updatedAt}
-                              catagory={data?.category}
-                              url={asPath}
-                            />
+          </Head>
+        </div>
+        <div className=" body__control" >
+          <div className="all__post__content__overlay">
+            <section className="banner-sec-wrap place-content-center">
+              <div className="banner__bg__sm__wrap relative w-full xl:h-[190px] lg:h-[180px] md:h-[180px] sm:h-[180px] xs:h-[170px]  overflow-hidden" style={{ background: `url('/images/pages-banner-svg/baseBanner.png')center center / cover no-repeat` }}>
+                {<h2 className=" absolute lg:text-[40px] md:text-[38px] sm:text-[35px] xs:text-[25px] text-[#F9A106]">{data?.category}</h2>}
+              </div>
+            </section>
+            <section className="all__post__main__content">
+              <div className="container">
+                <div className="lg:flex lg:flex-row">
+                  {(
+                    <div className="flex flex-col w-full relative z-50">
+                      {isdataFetch &&
+                        <>
+                          <div className="kobita__dsc__lft relative flex lg:mb-[110px] md:mb-[84px]">
+                            <div>
+                              <FullPost
+                                id={slug}
+                                content={data?.content}
+                                title={data?.title}
+                                writer={data?.writer}
+                                writer_id={data?.writer_id}
+                                image={selectedCoverImage}
+                                uploadedBy={profileName}
+                                writer_image={writerImage}
+                                profileName={profileName}
+                                updatedAt={data?.updatedAt}
+                                catagory={data?.category}
+                                url={asPath}
+                              />
+
+                            </div>
+                            <div>
+                              <button onMouse className="absolute  w-[35px] h-[35px] right-2 mt-[5px] text-white rounded-xl bg-orange-400" onClick={() => setIsModalOpen(true)}><i class="ri-book-read-fill text-[22px]"></i></button>
+                            </div>
+
+
+
 
                           </div>
-                          <div>
-                            <button onMouse className="absolute  w-[35px] h-[35px] right-2 mt-[5px] text-white rounded-xl bg-orange-400" onClick={() => setIsModalOpen(true)}><i class="ri-book-read-fill text-[22px]"></i></button>
+                          <div className="rating__share__wrap">
+                            <button
+                              className="facebook__share bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded flex items-center"
+                              onClick={() => window.open(`https://www.facebook.com/sharer/sharer.php?u=https://lekharpoka.com/post/${slug}`, '_blank')}>
+                              <i class="ri-facebook-circle-fill mr-[4px]"></i> শেয়ার করুন
+                            </button>
+
+                            {/* <ShareOnFacebook url={`lekharpoka.com/post/${slug}`} title={'লেখার পোকায় আপনাকে স্বাগতম'} image={''} /> */}
+                            <RatingComponent setRating={setRating} rating={rating} post_id={data?._id} />
                           </div>
+                        </>
+                      }
+                      {!isdataFetch &&
+                        <>
+                          <div className="text-black text-2xl mb-[75px] h-[100vh]">
+                            আপনার অনুসন্ধানকৃত লেখাটি পাওয়া যাচ্ছে না !
+                          </div>
+                        </>
+                      }
+                    </div>
 
+                  )
 
-
-
-                        </div>
-                        <div className="rating__share__wrap">
-                          <button
-                            className="facebook__share bg-blue-500 hover:bg-blue-700 text-white  py-2 px-4 rounded flex items-center"
-                            onClick={() => window.open(`https://www.facebook.com/sharer/sharer.php?u=https://lekharpoka.com/post/${slug}`, '_blank')}>
-                            <i class="ri-facebook-circle-fill mr-[4px]"></i> শেয়ার করুন
-                          </button>
-
-                          {/* <ShareOnFacebook url={`lekharpoka.com/post/${slug}`} title={'লেখার পোকায় আপনাকে স্বাগতম'} image={''} /> */}
-                          <RatingComponent setRating={setRating} rating={rating} post_id={data?._id} />
-                        </div>
-                      </>
-                    }
-                    {!isdataFetch &&
-                      <>
-                        <div className="text-black text-2xl mb-[75px] h-[100vh]">
-                          আপনার অনুসন্ধানকৃত লেখাটি পাওয়া যাচ্ছে না !
-                        </div>
-                      </>
-                    }
-                  </div>
-
-                )
-
-                }
-                {/* <div className="kobita__dsc__rgt lg:w-[30%]">
+                  }
+                  {/* <div className="kobita__dsc__rgt lg:w-[30%]">
               <Sidebar />
             </div> */}
+                </div>
               </div>
-            </div>
 
 
-          </section>
-          <ToastContainer />
+            </section>
+            <ToastContainer />
 
+          </div>
+
+
+          <ReaderModeModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
+            <section className="read__mod__sec__wrap py-[100px] pb-[470px]" onCopy={(e) => { e.preventDefault(); alert('এই ওয়েবসাইটের যেকোনো লেখা আমাদের অনুমতি ছাড়া কপি করলে আইনগত ব্যবস্থা গ্রহণ করা হবে।') }}>
+              <div className="read__mod__wrap">
+                <div className="read__mod__btn">
+                  <button className="w-[40px] h-[40px] text-white rounded-full bg-orange-400" onClick={readerModeClosehandler}><i class="ri-close-large-fill"></i></button>
+                </div>
+                <div className="read__mod__innr">
+                  <FullPostReaderMode
+                    title={data?.title}
+                    writer={data?.writer}
+                    writer_id={data?.writer_id}
+                    catagory={data?.category}
+                    content={data?.content}
+                  />
+                </div>
+              </div>
+            </section>
+
+          </ReaderModeModal>
+
+
+          {isAudioAvailable && (
+
+            <MusicPlayer songs={[{
+              id: data?._id,
+              title: data?.title,
+              src: `${apiBasePath}/${data?.audio?.slice(data.audio.indexOf("/") + 1)}`,
+              writer: data?.writer,
+              image: `${apiBasePath}/${writerImage?.slice(writerImage.indexOf("/") + 1)}`,
+
+            }]} />
+
+          )}
         </div>
 
-
-        <ReaderModeModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-          <section className="read__mod__sec__wrap py-[100px] pb-[470px]" onCopy={(e) => { e.preventDefault(); alert('এই ওয়েবসাইটের যেকোনো লেখা আমাদের অনুমতি ছাড়া কপি করলে আইনগত ব্যবস্থা গ্রহণ করা হবে।') }}>
-            <div className="read__mod__wrap">
-              <div className="read__mod__btn">
-                <button className="w-[40px] h-[40px] text-white rounded-full bg-orange-400" onClick={readerModeClosehandler}><i class="ri-close-large-fill"></i></button>
-              </div>
-              <div className="read__mod__innr">
-                <FullPostReaderMode
-                  title={data?.title}
-                  writer={data?.writer}
-                  writer_id={data?.writer_id}
-                  catagory={data?.category}
-                  content={data?.content}
-                />
-              </div>
-            </div>
-          </section>
-
-        </ReaderModeModal>
-
-
-        {isAudioAvailable && (
-
-          <MusicPlayer songs={[{
-            id: data?._id,
-            title: data?.title,
-            src: `${apiBasePath}/${data?.audio?.slice(data.audio.indexOf("/") + 1)}`,
-            writer: data?.writer,
-            image: `${apiBasePath}/${writerImage?.slice(writerImage.indexOf("/") + 1)}`,
-
-          }]} />
-
-        )}
-      </div>
-
-      </> : 
-      <>
-      
-      </>
-}
+      </> :
+        <>
+          <div className="text-black pt-[100px] pb-[500px] pl-[50px]">
+            <p>দুঃখিত লেখাটি পাওয়া যাচ্ছে না!</p>
+          </div>
+        </>
+      }
     </>
   );
 }
