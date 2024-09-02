f4eacb0aeaa30b01f0e25d4bae3b46fb0a8082b1
Fuad Hasan <47121920+fuad-hasan-safat@users.noreply.github.com>
2024-09-02 23:45:52 +0600

    hgf

--- components/common/loading.jsx
+++ components/common/loading.jsx
@@ -7,7 +7,7 @@ export default function Loading() {
 
       <div className="spinner"></div>
 
-      <p>Loading...</p>
+      <p className='min-h-[1900px] h-[2100px]'>Loading...</p>
       
     </div>
 
